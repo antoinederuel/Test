07c39a8f860181e933c248a27cfc15c668ca000c
antoinederuel <antoine.deruel@epitech.eu>
2025-12-02 21:49:36 +0100

    Debug: Add startup logger to check config

--- backend/src/main/java/com/example/todo/TodoApplication.java
+++ backend/src/main/java/com/example/todo/TodoApplication.java
@@ -1,13 +1,32 @@
 package com.example.todo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 
+    // --- LE MOUCHARD DE DEBUG ---
+    @Bean
+    CommandLineRunner runner(Environment env) {
+        return args -> {
+            System.out.println("=================================================");
+            System.out.println(">>> DÉMARRAGE DE L'APPLICATION (VERSION DEBUG) <<<");
+            
+            String mongoUri = env.getProperty("spring.data.mongodb.uri");
+            System.out.println("1. URI CONFIGURÉE DANS SPRING : " + mongoUri);
+            
+            String envVar = System.getenv("SPRING_DATA_MONGODB_URI");
+            System.out.println("2. VARIABLE D'ENVIRONNEMENT SYSTÈME : " + envVar);
+            
+            System.out.println("=================================================");
+        };
+    }
 }
